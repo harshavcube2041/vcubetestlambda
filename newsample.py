51c44acda9c8b6556f2861d356abf17fef203c65
harsha <harsha.gundapaneni@ivativ.com>
2022-08-18 08:28:55 +0530

    new code upload to github

--- newsample.py
+++ newsample.py
@@ -1,10 +1,4 @@
-# Creating a Multi-Dimensional List
-# (By Nesting a list inside a List)
-List = [['Geeks', 'For'], ['Geeks']]
-
-# accessing an element from the
-# Multi-Dimensional List using
-# index number
+List = [['vcube', 'For'], ['test']]
 print("Accessing a element from a Multi-Dimensional list")
 print(List[0][1])
 print(List[1][0])
